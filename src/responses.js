56eb40f235d6bd68aa7e34ede990c5800e2aff30
Justin Hall <justin.r.hall@gmail.com>
2016-12-10 14:25:07 -0700

    Document the responses module

--- src/responses.js
+++ src/responses.js
@@ -1,3 +1,15 @@
+/**
+ * Known potential responses from the remote API.
+ *
+ * Unfortunately, the API does not send a decent human-readable message back
+ * with each response, but they are documented on the website:
+ * https://haveibeenpwned.com/api/v2#ResponseCodes
+ *
+ * These objects simply provide a mapping between the HTTP response status code
+ * and the corresponding human-readable message so we can throw a more
+ * descriptive error for the consumer. (They are also leveraged in our tests.)
+ */
+
 export const OK = {
   status: 200,
 };
@@ -19,6 +31,11 @@ export const NOT_FOUND = {
     'not been pwned.',
 };
 
+/**
+ * This response has unique behavior. For some reason, the API includes a
+ * human-readable message in the response body for this one. Manually populating
+ * the message here purely for use in tests.
+ */
 export const TOO_MANY_REQUESTS = {
   status: 429,
   response: 'Rate limit exceeded, refer to acceptable use of the API: ' +
