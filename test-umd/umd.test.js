af2904ccdf2f4086821602fae96e644fa83d7841
Justin Hall <justin.r.hall@gmail.com>
2017-08-30 00:01:16 -0600

    Test the UMD build in headless Chrome (#17)
    * puppeteer is an optional dependency as it will not install on
      Node < 6 and hibp is still tested on Node 4

---
+++ test-umd/umd.test.js
@@ -0,0 +1,34 @@
+import path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import puppeteer from 'puppeteer';
+
+describe('UMD build', () => {
+  const hibpTest = `
+    if (typeof window.hibp !== 'object') throw new Error();
+    if (typeof window.hibp.breach !== 'function') throw new Error();
+    if (typeof window.hibp.breachedAccount !== 'function') throw new Error();
+    if (typeof window.hibp.breaches !== 'function') throw new Error();
+    if (typeof window.hibp.dataClasses !== 'function') throw new Error();
+    if (typeof window.hibp.pasteAccount !== 'function') throw new Error();
+    if (typeof window.hibp.pwnedPassword !== 'function') throw new Error();
+    if (typeof window.hibp.search !== 'function') throw new Error();
+  `;
+
+  let browser;
+
+  before(async () => {
+    browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  });
+
+  after(() => {
+    browser.close();
+  });
+
+  it('should expose the hibp namespace on the window object', async () => {
+    const page = await browser.newPage();
+    const testUrl = `file://${path.join(__dirname, 'index.html')}`;
+
+    await page.goto(testUrl);
+    return page.evaluate(hibpTest);
+  });
+});
