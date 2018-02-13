68ccb8a66e5e3fdf7f47a98c95919631814eb7a1
Justin Hall <justin.r.hall@gmail.com>
2018-02-13 14:24:54 -0700

    Upgrade rollup to 0.55.5

--- rollup.config.js
+++ rollup.config.js
@@ -32,15 +32,13 @@ const config = {
 if (process.env.NODE_ENV === 'production') {
   config.output.file = 'dist/hibp.min.js';
   config.plugins.push(
-    uglify(
-      {
-        compress: {
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-        },
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
       },
-    ), // eslint-disable-line comma-dangle
+    }), // eslint-disable-line comma-dangle
   );
 }
 
