88ccb62c9f44a48e156f172b3d0493ac37f72bd6
Justin Hall <justin.r.hall@gmail.com>
2018-02-24 20:52:45 -0700

    Drop support for Node < 6

--- .babelrc.js
+++ .babelrc.js
@@ -14,7 +14,7 @@ module.exports = {
           ? { node: 'current' }
           : {
               browsers: ['> 1%', 'last 2 versions'],
-              node: 4,
+              node: 6,
             },
       },
     ],
