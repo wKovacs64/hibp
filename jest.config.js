be6ebf371be9b6b509ac6f3d2f9e287ab21d979d
Justin Hall <justin.r.hall@gmail.com>
2018-04-06 15:12:19 -0600

    Ignore node_modules in jest coverage
    node_modules is ignored by default, but we specify our own
    coveragePathIgnorePatterns array.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/test/'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '/test/'],
   coverageReporters: ['html', 'json', 'lcov', 'text'],
   modulePaths: ['<rootDir>/src', '<rootDir>/test'],
   modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/es', '<rootDir>/lib'],
