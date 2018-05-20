48d7714a19ba2c6e424634145a7858181877984c
Justin Hall <justin.r.hall@gmail.com>
2018-05-20 11:19:55 -0600

    Replace rollup-plugin-uglify with rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 // Common
 const config = {
@@ -33,7 +33,7 @@ const config = {
 if (process.env.NODE_ENV === 'production') {
   config.output.file = 'dist/hibp.min.js';
   config.plugins.push(
-    uglify({
+    terser({
       compress: {
         pure_getters: true,
         unsafe: true,
