24ce72303523e11a014d402b8744fc6a47a7a4e0
Justin Hall <justin.r.hall@gmail.com>
2018-10-20 12:41:20 -0600

    Remove unnecessary @babel/plugin-transform-modules-commonjs

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,11 @@
 const test = process.env.NODE_ENV === 'test';
 
 module.exports = {
-  plugins: [test && '@babel/plugin-transform-modules-commonjs'].filter(Boolean),
   presets: [
     [
       '@babel/preset-env',
       {
-        modules: false,
+        modules: test ? 'commonjs' : false,
         targets: {
           browsers: ['> 1%', 'last 2 versions'],
           node: 6,
