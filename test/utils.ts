c0e154a766d03b684e82220b565ed0d2425bd3b5
Justin Hall <justin.r.hall@gmail.com>
2019-09-17 16:52:01 -0600

    test: move AxiosError to test utils
    It's not really a mock, we were just abusing Jest's mock resolution mechanism.

--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,18 @@
+import { HaveIBeenPwnedApiResponse } from '../src/api/haveibeenpwned/responses';
+import { PwnedPasswordsApiResponse } from '../src/api/pwnedpasswords/responses';
+
+type ApiResponse = HaveIBeenPwnedApiResponse | PwnedPasswordsApiResponse;
+
+/** @internal */
+export class AxiosError extends Error {
+  response: ApiResponse;
+
+  constructor(response: ApiResponse) {
+    super();
+    this.response = response;
+  }
+}
+
 export function mockResponse({
   headers = {},
   status = 200,
