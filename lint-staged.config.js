90055cfd74ce9d0586c20c22bacb584123a533aa
Justin Hall <justin.r.hall@gmail.com>
2020-01-19 18:29:21 -0700

    Update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,ts}': ['prettier --write', 'eslint --fix', 'git add'],
-  '*.{html,json,md,yml,yaml}': ['prettier --write', 'git add'],
+  '*.{js,ts}': ['prettier --write', 'eslint --fix'],
+  '*.{html,json,md,yml,yaml}': ['prettier --write'],
 };
