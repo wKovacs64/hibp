abd916cac04af124acfd0fddcd60481f2a4c9211
Justin Hall <justin.r.hall@gmail.com>
2020-05-13 19:25:11 -0600

    test: convert hibp export snapshot to inline snapshot

--- src/__tests__/hibp.test.ts
+++ src/__tests__/hibp.test.ts
@@ -2,6 +2,17 @@ import * as hibp from '../hibp';
 
 describe('hibp', () => {
   it('exports an object containing the advertised functions', () => {
-    expect(hibp).toMatchSnapshot();
+    expect(hibp).toMatchInlineSnapshot(`
+      Object {
+        "breach": [Function],
+        "breachedAccount": [Function],
+        "breaches": [Function],
+        "dataClasses": [Function],
+        "pasteAccount": [Function],
+        "pwnedPassword": [Function],
+        "pwnedPasswordRange": [Function],
+        "search": [Function],
+      }
+    `);
   });
 });
