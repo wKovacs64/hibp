ecbd59b46e85fd9491ea0b70034e0f8092fad549
Justin Hall <justin.r.hall@gmail.com>
2021-11-19 20:17:57 -0700

    ci(size-limit): update limits for size-limit v7
    size-limit v7 uses esbuild instead of webpack for bundling and for some
    reason the sizes are all a few hundred bytes larger.

--- .size-limit.js
+++ .size-limit.js
@@ -2,77 +2,77 @@ module.exports = [
   // Pre-bundled for Browser (UMD)
   {
     path: 'dist/browser/hibp.umd.js',
-    limit: '5.7 kB',
+    limit: '6.1 kB',
   },
   // Pre-bundled for Browser (ESM)
   {
     path: 'dist/browser/hibp.module.js',
-    limit: '4.6 kB',
+    limit: '4.7 kB',
   },
   // Bundled with Webpack (CJS)
   {
     path: 'dist/cjs/breach.js',
-    limit: '1.3 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/cjs/breachedAccount.js',
-    limit: '1.4 kB',
+    limit: '1.6 kB',
   },
   {
     path: 'dist/cjs/breaches.js',
-    limit: '1.4 kB',
+    limit: '1.6 kB',
   },
   {
     path: 'dist/cjs/dataClasses.js',
-    limit: '1.3 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/cjs/pasteAccount.js',
-    limit: '1.3 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/cjs/pwnedPassword.js',
-    limit: '4.1 kB',
+    limit: '4.6 kB',
   },
   {
     path: 'dist/cjs/pwnedPasswordRange.js',
-    limit: '1 kB',
+    limit: '1.1 kB',
   },
   {
     path: 'dist/cjs/search.js',
-    limit: '1.6 kB',
+    limit: '1.8 kB',
   },
   // Bundled with Webpack (ESM)
   {
     path: 'dist/esm/breach.mjs',
-    limit: '1.1 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/esm/breachedAccount.mjs',
-    limit: '1.1 kB',
+    limit: '1.6 kB',
   },
   {
     path: 'dist/esm/breaches.mjs',
-    limit: '1.1 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/esm/dataClasses.mjs',
-    limit: '1.1 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/esm/pasteAccount.mjs',
-    limit: '1.1 kB',
+    limit: '1.5 kB',
   },
   {
     path: 'dist/esm/pwnedPassword.mjs',
-    limit: '4 kB',
+    limit: '4.3 kB',
   },
   {
     path: 'dist/esm/pwnedPasswordRange.mjs',
-    limit: '1 kB',
+    limit: '1.2 kB',
   },
   {
     path: 'dist/esm/search.mjs',
-    limit: '1.3 kB',
+    limit: '1.7 kB',
   },
 ];
