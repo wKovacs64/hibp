06ff9031c612d58f0d75b018ae6308aa99858021
Justin Hall <justin.r.hall@gmail.com>
2022-11-28 10:13:14 -0700

    feat(deps): update eslint-plugin-wkovacs64 to v13.18.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,35 +9,6 @@ module.exports = {
     'jest/no-conditional-in-test': 'off',
     // doesn't work with vitest as it relies on jest version detection
     'jest/no-deprecated-functions': 'off',
-    // TODO: remove once added to eslint-plugin-wkovacs64
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '.*rc.{js,cjs,mjs}',
-          '*.{config,setup}.{js,cjs,mjs}',
-          '**/test.{js,jsx}',
-          '**/test-*.{js,jsx}',
-          '**/*{.,_}{test,spec}.{js,jsx}',
-          '**/test/**',
-          '**/tests/**',
-          '**/spec/**',
-          '**/__tests__/**',
-          '**/__mocks__/**',
-          '**/cypress/**',
-          '**/jest.config.{js,cjs,mjs}',
-          '**/jest.setup.{js,cjs,mjs}',
-          '**/setupTests.{js,cjs,mjs}',
-          '**/setupProxy.{js,cjs,mjs}',
-          '**/webpack.config.{js,cjs,mjs}',
-          '**/webpack.config.*.{js,cjs,mjs}',
-          '**/rollup.config.{js,cjs,mjs}',
-          '**/rollup.config.*.{js,cjs,mjs}',
-          '**/prettier.config.{js,cjs,mjs}',
-          '**/vitest.config.{js,ts}',
-        ],
-      },
-    ],
   },
   settings: {
     react: {
