da5af236c7e033a2b5f25189843bac835fb12373
Justin Hall <justin.r.hall@gmail.com>
2023-06-14 18:53:55 -0600

    test: replace Cypress with Playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['./src/**/*.test.{ts,tsx}'],
     setupFiles: './test/setup.ts',
     coverage: {
       exclude: [
