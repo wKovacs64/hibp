8d97621be34529dc13c6d9d418b3db0c64f168bf
Justin Hall <justin.r.hall@gmail.com>
2023-11-04 12:13:40 -0600

    fix: update the Breach model

--- src/api/haveibeenpwned/types.ts
+++ src/api/haveibeenpwned/types.ts
@@ -11,13 +11,15 @@ export interface Breach {
   ModifiedDate: string;
   PwnCount: number;
   Description: string;
-  LogoPath: string;
   DataClasses: string[];
   IsVerified: boolean;
   IsFabricated: boolean;
   IsSensitive: boolean;
   IsRetired: boolean;
   IsSpamList: boolean;
+  IsMalware: boolean;
+  IsSubscriptionFree: boolean;
+  LogoPath: string;
 }
 
 export interface Paste {
