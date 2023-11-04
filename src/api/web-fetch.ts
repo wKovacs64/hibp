99ce6fbfba7c5de0ea479cb849d286915097e231
Justin Hall <justin.r.hall@gmail.com>
2023-11-04 09:53:11 -0600

    chore: add comment regarding future removal of web-fetch

--- src/api/web-fetch.ts
+++ src/api/web-fetch.ts
@@ -1,3 +1,6 @@
+// This can probably be removed in favor of Node's native fetch once we drop
+// support for v18. https://x.com/ebey_jacob/status/1709975146939973909?s=20
+
 /* c8 ignore next */
 export default typeof window !== 'undefined' ? window.fetch : fetchWrapper;
 
