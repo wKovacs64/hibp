2d81456df438263da41ad6d65c0aad4b68597202
Justin Hall <justin.r.hall@gmail.com>
2026-08-15 14:07:27 -0600

    chore: fix lint warnings (#603)

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,3 +1,7 @@
 import { createConfig } from "@wkovacs64/oxlint-config";
 
-export default createConfig();
+export default createConfig({
+  rules: {
+    "typescript/no-unsafe-type-assertion": "off",
+  },
+});
